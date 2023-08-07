1f4d765a8fc98574b729619d36f465b69342eca4
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-08-07 13:24:02 +0300

    Hız ve satır değiştirme

--- alien.py
+++ alien.py
@@ -21,7 +21,14 @@ class Alien(Sprite):
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
 
+    # alienlerin kenarlara çarpıp çarpmadığını kontrol eder.
+    def check_edges(self): 
+        """ Return True if alien is at edge of screen. """
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
     def update(self):
         """ Move the alien to the right or left. """
-        self.x += self.settings.alien_speed # alienlerin hızını ayarlar.float değerleri tutabilir.
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction) # alienlerin hızını ayarlar.float değerleri tutabilir.
         self.rect.x = self.x
