eb2ae3285de71303dd22c61e6d314db481b4413d
Yusuf Yağcı <98044490+Yusufygc@users.noreply.github.com>
2023-08-12 10:53:47 +0300

    high score belirleme

--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,8 @@ class Scoreboard():
 
         # Prepare the initial score image.
         self.prep_score() # score'u hazırla
+        self.prep_high_score() # high score'u hazırla
+
 
 
     def prep_score(self):
@@ -29,9 +31,27 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20 # score'u sağ üst köşeye yerleştiriyoruz.
         self.score_rect.top = 20 # score'u sağ üst köşeye yerleştiriyoruz.
 
+    def prep_high_score(self):
+        """Turn the high score into a rendered image."""
+        high_score = round(self.stats.high_score, -1) # high score'u 10'luk basamağa yuvarlıyoruz.
+        high_score_str = "high score : {:,}".format(high_score) # high score'u stringe çeviriyoruz ve virgülden sonra 3 basamaklı sayılar için virgül koyuyoruz.
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color) # high score'u fonta göre render ediyoruz.
+
+        # Center the high score at the top of the screen.
+        self.high_score_rect = self.high_score_image.get_rect() # high score'un koordinatlarını alıyoruz.
+        self.high_score_rect.centerx = self.screen_rect.centerx # high score'u ekranın ortasına yerleştiriyoruz.
+        self.high_score_rect.top = self.score_rect.top # high score'u score'un üstüne yerleştiriyoruz.
+
 
     def show_score(self):
         """Draw score to the screen."""
         self.screen.blit(self.score_image, self.score_rect) # score'u ekrana yazdırıyoruz.
         # bu method score_rect in belirlediği koordinatlarda score_image'ı ekrana yazdırır.
+        self.screen.blit(self.high_score_image, self.high_score_rect) # high score'u ekrana yazdırıyoruz.
+
+    def check_high_score(self):
+        """Check to see if there's a new high score."""
+        if self.stats.score > self.stats.high_score: # eğer score high score'dan büyükse
+            self.stats.high_score = self.stats.score # high score'u score'a eşitle
+            self.prep_high_score() # high score'u güncelleriz.
    
